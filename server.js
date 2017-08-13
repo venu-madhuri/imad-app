87035da3c55a2ad125504c2d8eef767cca7bf45e
venu-madhuri <30875261+venu-madhuri@users.noreply.github.com>
2017-08-14 00:52:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 app.get('/article-one',function(req,res)
 {
-   res.send('article one will be sered here');
+   res.send('article one will be served here');
 });
 app.get('/article-two',function(req,res)
 {
-   res.send('article two will be sered here');
+   res.send('article two will be served here');
 });
 app.get('/article-three',function(req,res)
 {
-   res.send('article three will be sered here');
+   res.send('article three will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
